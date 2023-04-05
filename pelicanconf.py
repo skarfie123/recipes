2ccda21bfd64fa46e1c89ca19c94f5ee6d692119
skarfie123 <rahulpai@rocketmail.com>
2023-04-05 23:45:52 +0100

    init

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = "Rahul Pai"
+SITENAME = "Rahul Pai Recipes"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Europe/London"
+
+DEFAULT_LANG = "English"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Source", "https://github.com/skarfie123/recipes"),
+    ("Blog", "https://skarfie123.github.io"),
+    ("Rahul Pai Creations", "https://rahulpaicreations.github.io"),
+)
+
+# Social widget
+SOCIAL = (
+    ("skarfie123", "https://github.com/skarfie123"),
+    ("LinkedIn", "https://in.linkedin.com/in/r-pai"),
+    ("Rahul Pai", "https://www.youtube.com/@skarfie123"),
+    ("Rahul Pai Creations", "https://www.youtube.com/@rahulpaicreations"),
+)
+
+DEFAULT_PAGINATION = 10
+
+FILENAME_METADATA = r"(?P<date>\d{4}-\d{2}-\d{2})_(?P<title>.*)"
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
